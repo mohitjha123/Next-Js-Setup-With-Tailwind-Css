5cdbba3027a9b33b88c05298c57bfb4fe415646a
mohitjha123 <mohit.jha@bazaarnxt.com>
2023-06-23 16:53:18 +0530

    23th-jun Next Js SetUp With Tailwind Css

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-6xl font-bold text-gray-300">
+        NextJs With Tailwind Css
+      </h1>
+    </main>
+  );
+}
